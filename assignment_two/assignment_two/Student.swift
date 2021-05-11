edbca661252f8a97cd385407b84a8df8b57c4a97
Adam Scott <ajscott1@utas.edu.au>
2021-05-11 12:48:44 +1000

    First week of each class called
    Also sort function fo rstudent week in
    
    need to add navigation between weeks n that

---
+++ assignment_two/assignment_two/Student.swift
@@ -0,0 +1,8 @@
+//
+//  Student.swift
+//  assignment_two
+//
+//  Created by Swift Labourer on 11/5/21.
+//
+
+import Foundation
---
+++ assignment_two/assignment_two/Week.swift
@@ -0,0 +1,12 @@
+//
+//  Week.swift
+//  assignment_two
+//
+//  Created by Swift Labourer on 11/5/21.
+//
+
+
+import Firebase
+import FirebaseFirestoreSwift
+
+
